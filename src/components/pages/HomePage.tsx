e555aac01ba50611e1350b154c755936944cdd1c
kkamara <kamaracomputers@gmail.com>
2024-05-06 17:50:34 +0100

    Add HomePage.tsx

---
+++ src/components/pages/HomePage.tsx
@@ -0,0 +1,52 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>HomePage</Text>
+      <View style={styles.boxContainer}>
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+      </View>
+      <View
+        style={[
+          styles.boxContainer,
+          { marginTop: 200, flexDirection: "row", }
+        ]}
+      >
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 30,
+  },
+  title: {
+    fontWeight: "700",
+    fontSize: 24,
+  },
+  boxContainer: {
+    marginTop: 30,
+    flex: 1,
+    flexDirection: "column",
+  },
+  box: {
+    borderWidth: 1,
+    borderColor: "#000",
+    width: 50,
+    height: 50,
+  }
+});
+
+export default HomePage;
